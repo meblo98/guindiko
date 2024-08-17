615e50777fd97af367a24bb61d9aeaf383b410bd
Malcom <malcom70976@gmail.com>
2024-08-17 19:49:57 +0000

    integration listeRV,listeMentor, AccueilMentee

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AccueilComponent } from "./mente/accueil/accueil.component";
+import { ListeMentorsComponent } from "./mente/liste-mentors/liste-mentors.component";
+import { ListeRDVComponent } from "./mente/liste-rdv/liste-rdv.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AccueilComponent, ListeMentorsComponent, ListeRDVComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/mente/liste-mentors/liste-mentors.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-mentors',
+  standalone: true,
+  imports: [],
+  templateUrl: './liste-mentors.component.html',
+  styleUrl: './liste-mentors.component.css'
+})
+export class ListeMentorsComponent {
+
+}
