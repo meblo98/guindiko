914499e64831610bff61b247ef2ba795ead19458
meblo98 <lomouhamedelbachir@gmail.com>
2024-08-18 01:44:21 +0000

    integration des pages users et tableau de bord

--- src/app/admin/gestion-user/gestion-user.component.ts
+++ src/app/admin/gestion-user/gestion-user.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from "../layouts/sidebar/sidebar.component";
+import { SidebarNavigateComponent } from "../layouts/sidebar-navigate/sidebar-navigate.component";
 
 @Component({
   selector: 'app-gestion-user',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, SidebarNavigateComponent],
   templateUrl: './gestion-user.component.html',
   styleUrl: './gestion-user.component.css'
 })
