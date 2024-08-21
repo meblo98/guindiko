ba1544fe0b67edfc6a2884045e778b56141fc639
mamebassine <mamebassine06@gmail.com>
2024-08-21 18:59:51 +0000

    Creactions de mes differente composants besoins

---
+++ src/app/commentaire/post-commentaire/post-commentaire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-commentaire',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-commentaire.component.html',
+  styleUrl: './post-commentaire.component.css'
+})
+export class PostCommentaireComponent {
+
+}
---
+++ src/app/post/post-forum/post-forum.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-forum',
+  standalone: true,
+  imports: [],
+  templateUrl: './post-forum.component.html',
+  styleUrl: './post-forum.component.css'
+})
+export class PostForumComponent {
+
+}
